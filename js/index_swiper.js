6f103c6629a54dae5c131b6678cebc141da942fb
lgj628 <lgj628@126.com>
2018-03-18 23:59:48 +0800

    3-18

--- js/index_swiper.js
+++ js/index_swiper.js
@@ -2,8 +2,14 @@ $(function() {
 	var swiper = new Swiper('.swiper-container',{
 		direction : 'horizontal',
 		slidesPerView: 4,
+//		paginationClickable: true,
+//	    spaceBetween: 4,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
 //		pagination: '.swiper-pagination',
+        
+//        freeMode : true,
+        freeModeMomentumRatio : 4,
+        
 	});
 });
